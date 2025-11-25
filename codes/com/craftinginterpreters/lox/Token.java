5292e30e9556a2648ae7122152d38af92c8a45ec
cimi25012 <cimi25012@g.nihon-u.ac.jp>
2025-11-25 12:35:55 +0900

    add Scanner.java and Token.java

--- codes/com/craftinginterpreters/lox/Token.java
+++ codes/com/craftinginterpreters/lox/Token.java
@@ -1,24 +1,19 @@
-package codes.com.craftinginterpreters.lox;
+package codes.com.craftinginterpreters;
 
-import java.io.EOFException;
+public class Token {
+  final TokenType type;
+  final String lexeme;
+  final Object literal;
+  final int line; 
 
-public class Token.Type {
-    // Single-character tokens.
-  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+  Token(TokenType type, String lexeme, Object literal, int line) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
 
-  // One or two character tokens.
-  BANG, BANG_EQUAL,
-  EQUAL, EQUAL_EQUAL,
-  GREATER, GREATER_EQUAL,
-  LESS, LESS_EQUAL,
-
-  // Literals.
-  IDENTIFIER, STRING, NUMBER,
-
-  // Keywords.
-  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
-
-  EOF
+  public String toString() {
+    return type + " " + lexeme + " " + literal;
+  }
 }
