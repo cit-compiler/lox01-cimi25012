582a664a88330451c4d3436217fabf2cc6932ede
cimi25012 <cimi25012@g.nihon-u.ac.jp>
2026-01-13 12:25:38 +0900

    add assignment

--- codes/com/craftinginterpreters/lox/Interpreter.java
+++ codes/com/craftinginterpreters/lox/Interpreter.java
@@ -3,6 +3,7 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object> ,Stmt.Visitor<Void>{
+  private Environment environment = new Environment();
   void interpret(List<Stmt> statements) {
       try {
         for (Stmt statement : statements) {
@@ -66,6 +67,18 @@ class Interpreter implements Expr.Visitor<Object> ,Stmt.Visitor<Void>{
     return null;
   }
 
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
+  }
+
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr) {
+    Object value = evaluate(expr.value);
+    environment.assign(expr.name, value);
+    return value;
+  }
+
   private void checkNumberOperand(Token operator, Object operand) {
     if (operand instanceof Double) return;
     throw new RuntimeError(operator, "Operand must be a number.");
@@ -95,6 +108,17 @@ class Interpreter implements Expr.Visitor<Object> ,Stmt.Visitor<Void>{
   public Void visitPrintStmt(Stmt.Print stmt) {
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
     return null;
   }
 
